0495de652103c4cba5359543c8c4db50f5750e39
lukektx <lukektx@gmail.com>
2023-10-24 14:11:04 -0500

    started blog project

---
+++ part4/blogList/models/blog.js
@@ -0,0 +1,22 @@
+/* eslint no-underscore-dangle: 0 */
+/* eslint no-param-reassign: 0 */
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model('Blog', blogSchema);
